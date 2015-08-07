e81907caa015a7a094ab6fda180078689b6522da
jcolebristol <jamie@wearecreatrix.com>
2015-08-07 15:54:38 +0100

    automated e-mail configured
    waiting for the password for sendgrid

--- email-automation/index.php
+++ email-automation/index.php
@@ -21,18 +21,18 @@ function sendemail($to, $from, $subject, $html){
 }
 
 function getinfo($temp){
-	$url = "http://careselector-core.herokuapp.com/api/providers/18773";
+	$url = "http://careselector-core.herokuapp.com/api/providers/for-emails";
 	$json = file_get_contents($url);
 	$json_data = json_decode($json, true);
 	$template = $temp;
-	if($json_data["data"]["terms_agreed"] == true)  {
+	if($json_data["data"]["terms_agreed"] == false)  {
 		$template = str_replace("%ORGANIZATION%", $json_data["data"]["name"], $template);
-		$template = str_replace("%TIMES_SEARCHED%", "64", $template);
-		$template = str_replace("%TIMES_SHORTLISTED%", "27", $template);
+		$template = str_replace("%TIMES_SEARCHED%", $json_data["data"]["no_of_searched"], $template);
+		$template = str_replace("%TIMES_SHORTLISTED%", $json_data["data"]["no_of_shortlisted"], $template);
 		$template = str_replace("%COMPETITOR1%", "Little Haven", $template);
 		$template = str_replace("%COMPETITOR2%", "Highlands Borders Care Home", $template);
 		$template = str_replace("%COMPETITOR3%", "Belmont Grange Nursing and Residential Home", $template);
-		sendemail("raf@wearecreatrix.com", "info@careselector.com", $json_data["data"]["name"] . " appeared in 64 searches", $template);
+		sendemail("raf@wearecreatrix.com", "info@careselector.com", $json_data["data"]["name"] . " appeared in " . $json_data["data"]["no_of_searched"] . "searches", $template);
 	} 
 	
 }
